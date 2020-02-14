806f752d592d279eb992ee29ccd77f512f35af02
RussellSB <r.sammutbonnici@gmail.com>
2020-02-14 20:33:17 +0100

    Re-adjusted displacement
    No longer dependant on bboxes at all!

--- pose_estimation.py
+++ pose_estimation.py
@@ -38,14 +38,11 @@ estimator.hybridize()
 #==================================================================================
 #                                   Methods
 #==================================================================================
-# Currently for debugging, seeing that everything is spotted well
-def plot_debug(img, coords, confidence, class_ids, bboxes, scores,
-                   box_thresh=0.5, keypoint_thresh=0.2):
+# Currently for debugging, seeing that everything is interpreted correctly
+def plot_debug(img, coords, confidence, scores, keypoint_thresh=0.2):
 
     if isinstance(coords, mx.nd.NDArray):
         coords = coords.asnumpy()
-    if isinstance(bboxes, mx.nd.NDArray):
-        bboxes = bboxes.asnumpy()
     if isinstance(scores, mx.nd.NDArray):
         scores = scores.asnumpy()
     if isinstance(confidence, mx.nd.NDArray):
@@ -56,12 +53,6 @@ def plot_debug(img, coords, confidence, class_ids, bboxes, scores,
                    [5, 6], [5, 7], [7, 9], [6, 8], [8, 10],
                    [5, 11], [6, 12], [11, 12],
                    [11, 13], [12, 14], [13, 15], [14, 16]]
-
-    bbox_first = bboxes[0][0]
-    x_min = bbox_first[0]
-    y_min = bbox_first[1]
-    x_max = bbox_first[2]
-    y_max= bbox_first[3]
 
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1) # img.shape[1], img.shape[0] (BELOW)
@@ -74,10 +65,9 @@ def plot_debug(img, coords, confidence, class_ids, bboxes, scores,
 
     for cm_ind, jp in zip(colormap_index, joint_pairs):
         if joint_visible[i, jp[0]] and joint_visible[i, jp[1]]:
-
-            ax.plot(pts[jp, 0] - x_min, y_max - pts[jp, 1],
+            ax.plot(pts[jp, 0], img.shape[0] - pts[jp, 1],
                     linewidth=3.0, alpha=0.7, color=plt.cm.cool(cm_ind))
-            ax.scatter(pts[jp, 0] - x_min, y_max - pts[jp, 1], s=20)
+            ax.scatter(pts[jp, 0], img.shape[0] - pts[jp, 1], s=20)
 
     return ax
 
@@ -95,6 +85,7 @@ def video_to_listPose(vid):
         # Object detection
         x, frame = gcv.data.transforms.presets.yolo.transform_test(frame)  # short=512, max_size=350
         class_IDs, scores, bounding_boxs = detector(x.as_in_context(ctx))
+        # Stores xyxy of the bounding box of the first frame
 
         # Pose estimation
         pose_input, upscale_bbox = detector_to_alpha_pose(frame, class_IDs, scores, bounding_boxs,
@@ -107,17 +98,16 @@ def video_to_listPose(vid):
 
         img = cv_plot_keypoints(frame, pred_coords, confidence, class_IDs, bounding_boxs, scores,
                                 box_thresh=0.5, keypoint_thresh=0.2)
-        #ax = plot_debug(img, pred_coords, confidence, class_IDs, bounding_boxs, scores, box_thresh=0.5,
-                     #   keypoint_thresh=0.2)
+        ax = plot_debug(img, pred_coords, confidence, scores, keypoint_thresh=0.2)
         cv_plot_image(img)
-        #plt.show()
+        plt.show()
         cv2.waitKey(1)
     cap.release()
 
 # Given two videos it will output the json describing all poses in both videos
 def videos_to_jsonPose(vidSide, vidFront):
     video_to_listPose(vidSide)
-    #frontView_list = video_to_listPose(vidFront)
+    #video_to_listPose(vidFront)
     #TODO: Ensure data lists are of same size
 
 #==================================================================================
--- visualizer.py
+++ visualizer.py
@@ -8,3 +8,46 @@
 #               extraction.
 #----------------------------------------------------------------------------------
 #==================================================================================
+
+# TODO: Scrap, and make version that works strictly with json
+def plot_debug(img, coords, confidence, class_ids, bboxes, scores,
+                   box_thresh=0.5, keypoint_thresh=0.2):
+
+    if isinstance(coords, mx.nd.NDArray):
+        coords = coords.asnumpy()
+    if isinstance(bboxes, mx.nd.NDArray):
+        bboxes = bboxes.asnumpy()
+    if isinstance(scores, mx.nd.NDArray):
+        scores = scores.asnumpy()
+    if isinstance(confidence, mx.nd.NDArray):
+        confidence = confidence.asnumpy()
+
+    joint_visible = confidence[:, :, 0] > keypoint_thresh
+    joint_pairs = [[0, 1], [1, 3], [0, 2], [2, 4],
+                   [5, 6], [5, 7], [7, 9], [6, 8], [8, 10],
+                   [5, 11], [6, 12], [11, 12],
+                   [11, 13], [12, 14], [13, 15], [14, 16]]
+
+    bbox_first = bboxes[0][0]
+    x_min = bbox_first[0]
+    y_min = bbox_first[1]
+    x_max = bbox_first[2]
+    y_max= bbox_first[3]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1) # img.shape[1], img.shape[0] (BELOW)
+    ax.set(xlim=(0, img.shape[1]), ylim = (0, img.shape[0])) # setting width and height of plot
+    #ax.invert_yaxis()
+
+    i = scores.argmax() # gets index of most confident bbox estimation
+    colormap_index = np.linspace(0, 1, len(joint_pairs))
+    pts = coords[i]
+
+    for cm_ind, jp in zip(colormap_index, joint_pairs):
+        if joint_visible[i, jp[0]] and joint_visible[i, jp[1]]:
+
+            ax.plot(pts[jp, 0] - x_min, y_max - pts[jp, 1],
+                    linewidth=3.0, alpha=0.7, color=plt.cm.cool(cm_ind))
+            ax.scatter(pts[jp, 0] - x_min, y_max - pts[jp, 1], s=20)
+
+    return ax
