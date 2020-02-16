26152b0e48acebea2fc9487e5c81b4072ec98261
RussellSB <r.sammutbonnici@gmail.com>
2020-02-16 16:13:20 +0100

    Photo extraction
    For now works by plotting poses for each frame and saving each frame in a specified folder with the frame count as the title, later can make it work more fluently in python with gifs

--- visualizer.py
+++ visualizer.py
@@ -12,11 +12,10 @@
 #==================================================================================
 #                                   Imports
 #==================================================================================
-import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib
-import imageio
+import matplotlib.pyplot as plt
 import json
+
 
 #==================================================================================
 #                                   Constants
@@ -25,39 +24,49 @@ joint_pairs = [[0, 1], [1, 3], [0, 2], [2, 4],
                    [5, 6], [5, 7], [7, 9], [6, 8], [8, 10],
                    [5, 11], [6, 12], [11, 12],
                    [11, 13], [12, 14], [13, 15], [14, 16]]
+colormap_index = np.linspace(0, 1, len(joint_pairs))
 
 #==================================================================================
 #                                   Methods
 #==================================================================================
-
-# Plots the pose of one frame, and returns axis
-def plot_pose(data, dim):
+# Saves every pose frame of the video
+def save_poses(data, dim, path, limit):
+    i = 1
     for frame in data:
-        fig = plt.figure()
-        ax = fig.add_subplot(1, 1, 1)
+        fig, ax = plt.subplots()
         ax.set(xlim=(0, dim[0]), ylim=(0, dim[1]))  # setting width and height of plot
-        colormap_index = np.linspace(0, 1, len(joint_pairs))
+
         for cm_ind, jp in zip(colormap_index, joint_pairs):
             joint1 = frame[jp[0]]
             joint2 = frame[jp[1]]
-            if(joint1 > [-1, -1] and joint2 > [-1,-1]):
-                print(joint1, joint2)
+            if (joint1 > [-1, -1] and joint2 > [-1, -1]):
                 x = [joint1[0], joint2[0]]
                 y = [joint1[1], joint2[1]]
                 ax.plot(x, y, linewidth=3.0, alpha=0.7, color=plt.cm.cool(cm_ind))
                 ax.scatter(x, y, s=20)
-        plt.show()
+        filename = path + str(i) + '.png'
+        plt.savefig(filename)
+        if(i == limit): break
+        i += 1
 
-# Makes a gif out of what is described in the jsonFile
-def jsonPose_to_gif(jsonFile):
-    with open('test10.json', 'r') as f:
+# Makes a collection of figures out of what is described in the jsonFile
+def jsonPose_to_pics(jsonFile, path):
+    with open(jsonFile, 'r') as f:
         jsonPose = json.load(f)
+
+    lenF = jsonPose[0]['lenF']
+    lenS = jsonPose[0]['lenS']
+    limit = min(lenF, lenS)
 
     dataS = jsonPose[0]['dataS']
     dimS = jsonPose[0]['dimS']
+    path1 = path + jsonPose[0]['id'] + '-S/'
+    save_poses(dataS, dimS, path1, limit)
 
     dataF = jsonPose[0]['dataF']
     dimF = jsonPose[0]['dimF']
+    path2 = path + jsonPose[0]['id'] + '-F/'
+    save_poses(dataF, dimF, path2, limit)
 
-
-#jsonPose_to_gif('test10.json')
+path = '../Test/GIF/'
+jsonPose_to_pics('test.json', path)
