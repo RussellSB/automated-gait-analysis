289ba883f934ad0aa10d2fb5a5f024d003125880
RussellSB <r.sammutbonnici@gmail.com>
2020-02-18 10:23:28 +0100

    Hip flex fix
    Should be fixed.... doesn't look exactly like the normal of 44 males, but I think it's good enough.... To be tested with other participants

--- feature_extraction.py
+++ feature_extraction.py
@@ -38,7 +38,7 @@ ptID = {
 #                                   Methods
 #==================================================================================
 # Calculates joint angle of knee, in terms of flexion and extension
-def calc_knee_angle_1(joint1, joint2, joint3):
+def calc_knee_angle(joint1, joint2, joint3):
     # Identifying joint positions
     a = np.array(joint1)
     b = np.array(joint2) # Main joint
@@ -53,47 +53,41 @@ def calc_knee_angle_1(joint1, joint2, joint3):
     angle = np.arccos(cosine_angle)
     return np.degrees(angle)
 
-# Calculates joint angle of knee, in terms of abduction and adduction
-def calc_knee_angle_2(joint1, joint2, joint3, count):
+# Calculates joint angle of hip, in terms of flexion and extension
+def calc_hip_angle(hip, knee):
     # Identifying joint positions
-    a = np.array(joint1)
-    b = np.array(joint2) # Main joint
-    c = np.array(joint3)
+    a = np.array(hip) # Main joint
+    b = np.array(knee)
 
     # Compute vectors from main joint
-    ba = a - b
-    m_ba = - ba
-    bc = c - b
+    ab = b - a
+    m_N = np.array([0,-1])
 
-    cosine_angle = np.dot(m_ba, bc) / (np.linalg.norm(m_ba) * np.linalg.norm(bc))
+    cosine_angle = np.dot(ab, m_N) / (np.linalg.norm(ab) * np.linalg.norm(m_N))
     angle = np.arccos(cosine_angle)
-    print(count)
     return np.degrees(angle)
 
 # Traversing through pose to debug
-def knee_angles(dataS, dataF, dim, limit=1000):
+def side_angles(dataS, dim, limit=1000):
 
     knee_FlexExt_L = []
     knee_FlexExt_R = []
-    knee_AbducAdduc_L = []
-    knee_AbducAdduc_R = []
+    hip_FlexExt_L = []
+    hip_FlexExt_R = []
 
     red = "#FF4A7E"
     blue = "#72B6E9"
 
+    count = 1
+    frames = []
+
     fig, ax = plt.subplots(2, 1)
-    ax[0].set_title('Knee Ab/Adduction')
+    ax[0].set_title('Hip Flex / Extension')
     ax[1].set_xlabel('Frame (count)')
     ax[1].set_ylabel(r"${\Theta}$ (degrees)")
     ax[0].set(xlim=(0, dim[0]), ylim=(0, dim[1]))
-    ax[1].set(xlim=(0, len(dataS)), ylim=(-20, 80))
+    ax[1].set(xlim=(0, len(dataS)), ylim=(-20, 60))
 
-    '''
-    count = 1
-    frames = []
-    # =======================================================
-    #           Knee Flexion / Extension
-    # =======================================================
     for pose in dataS:
         frames.append(count)
 
@@ -101,80 +95,47 @@ def knee_angles(dataS, dataF, dim, limit=1000):
         hip_L = pose[ptID['hip_L']]
         knee_L = pose[ptID['knee_L']]
         ankle_L = pose[ptID['ankle_L']]
-        if(hip_L == [-1,-1] or knee_L == [-1,-1] or ankle_L == [-1,-1]): break
+        shoulder_L = pose[ptID['shoulder_L']]
+        #if(hip_L == [-1,-1] or knee_L == [-1,-1] or ankle_L == [-1,-1] or shoulder_L == [-1, -1]): break
         
-        x = [hip_L[0], knee_L[0], ankle_L[0]]
-        y = [hip_L[1], knee_L[1], ankle_L[1]]
-        angle = calc_knee_angle_1(hip_L, knee_L, ankle_L)
+        x = [shoulder_L[0], hip_L[0], knee_L[0], ankle_L[0]]
+        y = [shoulder_L[1], hip_L[1], knee_L[1], ankle_L[1]]
+        angle = calc_knee_angle(hip_L, knee_L, ankle_L)
         knee_FlexExt_L.append(angle)
+        angle = calc_hip_angle(hip_L, knee_L)
+        hip_FlexExt_L.append(angle)
+
         ax[0].scatter(x, y, s=20, color=red)
         ax[0].plot(x, y, color=red)
-        ax[1].plot(frames, knee_FlexExt_L, color=red)
+        ax[1].plot(frames, hip_FlexExt_L, color=red)
 
         #Right
         hip_R = pose[ptID['hip_R']]
         knee_R = pose[ptID['knee_R']]
         ankle_R = pose[ptID['ankle_R']]
-        if(hip_R == [-1,-1] or knee_R == [-1,-1] or ankle_R == [-1,-1]): break
-        
-        x = [hip_R[0], knee_R[0], ankle_R[0]]
-        y = [hip_R[1],knee_R[1], ankle_R[1]]
-        angle = calc_knee_angle_1(hip_R, knee_R, ankle_R)
+        shoulder_R = pose[ptID['shoulder_R']]
+        #if(hip_R == [-1,-1] or knee_R == [-1,-1] or ankle_R == [-1,-1] or shoulder_R == [-1, -1]): break
+
+        x = [shoulder_R[0], hip_R[0], knee_R[0], ankle_R[0]]
+        y = [shoulder_R[1], hip_R[1], knee_R[1], ankle_R[1]]
+        angle = calc_knee_angle(hip_R, knee_R, ankle_R)
         knee_FlexExt_R.append(angle)
+        angle = calc_hip_angle(hip_R, knee_R)
+        hip_FlexExt_R.append(angle)
+
         ax[0].scatter(x, y, s=20, color=blue)
         ax[0].plot(x, y, color=blue)
-        ax[1].plot(frames, knee_FlexExt_R, color=blue)
+        ax[1].plot(frames, hip_FlexExt_R, color=blue)
 
         if(count == limit): break
         count += 1
-    knee_FlexExt = [knee_FlexExt_L, knee_FlexExt_R]
-    '''
 
-    # =======================================================
-    #           Knee Abduction / Adduction
-    # =======================================================
-    count = 1
-    frames = []
-    for pose in dataF:
-        frames.append(count)
-
-        # Left
-        hip_L = pose[ptID['hip_L']]
-        knee_L = pose[ptID['knee_L']]
-        ankle_L = pose[ptID['ankle_L']]
-        if(hip_L == [-1,-1] or knee_L == [-1,-1] or ankle_L == [-1,-1]): break
-
-        x = [hip_L[0], knee_L[0], ankle_L[0]]
-        y = [hip_L[1], knee_L[1], ankle_L[1]]
-        angle = calc_knee_angle_2(hip_L, knee_L, ankle_L, count)
-        knee_AbducAdduc_L.append(angle)
-        ax[0].scatter(x, y, s=20, color=red)
-        ax[0].plot(x, y, color=red)
-        ax[1].plot(frames, knee_AbducAdduc_L, color=red)
-
-        # Right
-        hip_R = pose[ptID['hip_R']]
-        knee_R = pose[ptID['knee_R']]
-        ankle_R = pose[ptID['ankle_R']]
-        if (hip_R == [-1, -1] or knee_R == [-1, -1] or ankle_R == [-1, -1]): break
-
-
-        x = [hip_R[0], knee_R[0], ankle_R[0]]
-        y = [hip_R[1], knee_R[1], ankle_R[1]]
-        angle = calc_knee_angle_2(hip_R, knee_R, ankle_R, count)
-        knee_AbducAdduc_R.append(angle)
-        ax[0].scatter(x, y, s=20, color=blue)
-        ax[0].plot(x, y, color=blue)
-        ax[1].plot(frames, knee_AbducAdduc_R, color=blue)
-
-        if (count == limit): break
-        count += 1
-    knee_AbducAdduc = [knee_AbducAdduc_L, knee_AbducAdduc_R]
     plt.show()
-    return knee_AbducAdduc
+    knee_FlexExt = [knee_FlexExt_L, knee_FlexExt_R]
+    #return knee_FlexExt
 
 # Makes a collection of figures out of what is described in the jsonFile
-def parse_jsonPose(jsonFile, path):
+def parse_jsonPose(jsonFile):
     with open('test.json', 'r') as f:
         jsonPose = json.load(f)
 
@@ -184,9 +145,4 @@ def parse_jsonPose(jsonFile, path):
 
     dataS = jsonPose[0]['dataS']
     dimS = jsonPose[0]['dimS']
-    dataF = jsonPose[0]['dataF']
-    dimF = jsonPose[0]['dimF']
-    knee_AbducAdduc = knee_angles(dataS, dataF, dimS, limit)
-
-path = '../Test/GIF/'
-parse_jsonPose('test.json', path)
+    side_angles(dataS, dimS, limit)
