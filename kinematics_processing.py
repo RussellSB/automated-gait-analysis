bff53833f68f100dbdd05383e69cec8048bbb286
RussellSB <r.sammutbonnici@gmail.com>
2020-02-23 22:35:34 +0100

    Updated demos
    Sample pictures to demo process

--- kinematics_processing.py
+++ kinematics_processing.py
@@ -275,7 +275,7 @@ def plot_avg_gcLR(avg_LR, title, yrange, plotSep):
 #==================================================================================
 #                                   Main
 #==================================================================================
-with open('../Test2/test.json', 'r') as f:
+with open('../Test/test.json', 'r') as f:
     jsonPose = json.load(f)
 
 dataS = jsonPose[0]['dataS']
@@ -284,7 +284,7 @@ dataF = jsonPose[0]['dataF']
 dimF = jsonPose[0]['dimF']
 lenS = jsonPose[0]['lenS']
 
-with open('../Test2/test_angles.json', 'r') as f:
+with open('../Test/test_angles.json', 'r') as f:
     jsonAngles = json.load(f)
 raw_angles = jsonAngles[0]
 
