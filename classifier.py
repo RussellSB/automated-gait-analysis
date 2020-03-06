8102616a3e0ae9fbed4927da55ff8dc1b3fbb3ca
RussellSB <r.sammutbonnici@gmail.com>
2020-03-06 22:36:01 +0100

    CNN Attempt

--- classifier.py
+++ classifier.py
@@ -11,7 +11,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.font_manager
 from sklearn.model_selection import train_test_split
-from sklearn import svm
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+from matplotlib import pyplot
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Dropout
+from keras.layers.convolutional import Conv1D, MaxPooling1D
+
 
 #==================================================================================
 #                                   Methods
@@ -40,21 +46,59 @@ def evaluate_lr_summary(classifier, X_test, y_test):
     disp.figure_.suptitle("")
     print("Confusion matrix:\n%s" % disp.confusion_matrix)
     plt.show()
+    plt.close()
+
+# Evaluates the dense neural network model w.r.t confusion matrix and ground-truth metrics
+def evaluate_nn_summary(model, X_test, y_test):
+    import matplotlib.pyplot as plt
+    from sklearn import metrics
+    import numpy as np
+    predictions = model.predict(X_test)
+    predictedClasses = np.where(predictions > 0.5, 1, 0)
+    cm = metrics.confusion_matrix(y_test, predictedClasses)
+    print('cm-nn:', cm)
+    plt.imshow(cm, cmap=plt.cm.Blues)
+    plt.xlabel("Predicted labels")
+    plt.ylabel("True labels")
+    plt.xticks([],[])
+    plt.yticks([],[])
+    plt.colorbar()
+    plt.show()
+    print('Classification Report NN:')
+    print(metrics.classification_report(y_test, predictedClasses))
 
 #==================================================================================
 #                                   Main
 #==================================================================================
-with open('..\\classifier_data\\data_na.pickle', 'rb') as f:
-    data_na = pickle.load(f)
-with open('..\\classifier_data\\labels_na.pickle', 'rb') as f:
+with open('..\\classifier_data\\data.pickle', 'rb') as f:
+    data = pickle.load(f)
+with open('..\\classifier_data\\labels_gen.pickle', 'rb') as f:
     labels = pickle.load(f)
 
-X = [x[0] for x in data_na]
-y = labels
+X = np.array([np.array(x).transpose() for x in data])  # (samples, time-steps, features)
+y = np.array(labels)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42, shuffle=True)
-lr = lr(X_train, X_test, y_train, y_test)
-evaluate_lr_summary(lr, X_test, y_test)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
 
+###
+verbose = 1
+epochs = 10
+batch_size = 32
+n_outputs = 2
+n_timesteps, n_features = X.shape[1], X.shape[2]
 
+model = Sequential()
+model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(n_timesteps, n_features)))
+model.add(Conv1D(filters=64, kernel_size=4, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(1, activation='relu'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
 
+# fit network
+model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, verbose=verbose)
+# evaluate model
+_, accuracy = model.evaluate(X_test, y_test, batch_size=batch_size, verbose=0)
+print(accuracy)
+evaluate_nn_summary(model, X_test, y_test)
